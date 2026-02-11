0c5a200a3a62b315418910bed64e2e7ba2a75c29
saadkhi <saadalioffic@gmail.com>
2026-02-12 01:38:16 +0500

    Portfolio files added

--- frontend/src/pages/Login.jsx
+++
@@ -1,8 +0,0 @@
-import Form from "../components/Form"
-import "../styles/Auth.css"
-
-function Login() {
-    return <div className="page-container"><Form route="/api/token/" method="login" /></div>;
-}
-
-export default Login
--- frontend/src/pages/Register.jsx
+++
@@ -1,8 +0,0 @@
-import Form from "../components/Form"
-import "../styles/Auth.css"
-
-function Register() {
-    return <div className="page-container"><Form route="/api/user/register/" method="register" /></div>;
-}
-
-export default Register;
