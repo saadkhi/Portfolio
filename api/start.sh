e50aea8f0141f6337f5ce90e03dc8837fe66de26
saadkhi <saadalioffic@gmail.com>
2026-03-06 03:09:24 +0500

    post deployment change 43 for all railway deployment

--- api/start.sh
+++ api/start.sh
@@ -10,13 +10,13 @@ npm run build
 cd ..
 
 echo "Running database migrations..."
-python3 manage.py migrate --noinput
+python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn portfolio_core.wsgi:application \
+gunicorn portfolio_core.wsgi:application \
   --bind 0.0.0.0:$PORT \
   --workers 1 \
   --threads 2 \
